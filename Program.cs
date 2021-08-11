0f85877ecbcdc74daf5a6ca50c8644e6dc3c2677
0-Chan <kyc4421194@gmail.com>
2021-08-11 13:59:51 +0900

    Parse DateTime within a function.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿using System.Linq;  // 자주 쓰는 친구들만 using
+﻿using System.Linq;
 
-namespace CombineCSV        // 재사용성을 고려할 것
+namespace CombineCSV
 {
 	class Program
 	{
-        public enum setType
+        public enum CsvCode
         {
-            Gap = 1,
+            Gap,
             Glass,
             Vehicle
 		}
 
-        public enum carCode
+        public enum CarCode // NO Enum with CarCode!
         {
             CN7,
             DN8,
@@ -19,195 +19,249 @@ namespace CombineCSV        // 재사용성을 고려할 것
             DH
 		}
 
- //       static System.Collections.Generic.List<string> SearchCSV(string logPath, setType type, carCode code = carCode.ALL);
- //       static void CombineCSVFiles(System.Collections.Generic.List<string> csvPaths, string resultPath);
-        
         static void Main(string[] args)
 		{
-            string logPath = @"D:\\CombineCSV\\VALUES\\";
-            string name = string.Empty;
-            string input = string.Empty;
-            int flag = 0;
+            string logPath = "/home/cody/workspace/VALUES/";
+            string dest = string.Empty;
+            System.Collections.Generic.List<string> csvPaths = null;
+            bool passFlag = false;
+            bool checker = true;
 
             if (System.IO.Directory.Exists(logPath))
             {
-                // 기간 입력
-                System.Console.WriteLine("Please enter the date. (yyyyMMdd) \n");
-
+                System.Console.WriteLine("Please enter the date in 'yyyyMMdd' type.");
                 System.Console.Write("Start Date :");
-                string date1 = System.Console.ReadLine();
-                System.DateTime dt1 ;                
-                System.DateTime.TryParseExact(date1, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out dt1);
-
+                string startInput = System.Console.ReadLine();
                 System.Console.Write("End Date :");
-                string date2 = System.Console.ReadLine();
-                System.DateTime dt2;
-                System.DateTime.TryParseExact(date2, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out dt2);
+                string endInput = System.Console.ReadLine();
 
-                System.Console.WriteLine("\n start : {0} \n end : {1}", date1, date2);
-                System.Console.WriteLine("\n start : {0} \n end : {1}", dt1, dt2);
+                checker = System.DateTime.TryParseExact(startInput, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out System.DateTime startDT);
+                checker = System.DateTime.TryParseExact(endInput, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out System.DateTime endDT);
 
-                System.Console.WriteLine("\n What values do you want to combine?");
+                if (checker == false)
+                {
+                    System.Console.WriteLine("DateTime Parse failed.");
+                    System.Environment.Exit(1);
+                }
+
+                System.Console.WriteLine("\n Which values do you want to combine?");
                 do
                 {
-                    System.Console.Write("1. Gap // 2. Glass // 3. Vehicle (Enter a number) \n> ");
-                    input = System.Console.ReadLine();
-                    
-                    var myType = (setType)System.Enum.Parse(typeof(setType), input);
-
-                    System.Collections.Generic.List<string> csvPaths = null;
-
-                    System.Console.WriteLine("Input type : " + myType);
+                    System.Console.Write("1. Gap // 2. Glass // 3. Vehicle (Enter a number.)\n>");
+                    int input = System.Convert.ToInt32(System.Console.ReadLine());
+                    input = input - 1;
+                    var myType = (CsvCode)input;
 
                     switch (myType)
                     {
-                        case setType.Gap:
-                            System.Console.WriteLine("Combining 1.Gap setType CSV files...");
-                            csvPaths = SearchCSV(logPath, date1, date2, carCode.CN7, setType.Gap);
-//                          CombineCSVFiles(csvPaths);
-                            flag = 1;
+                        case CsvCode.Gap:
+                            System.Console.WriteLine("Combining 1.Gap CSV files...");
+                            System.Console.WriteLine("1. CN7");
+                            csvPaths = SearchCSV(logPath, startDT, endDT, CarCode.CN7, CsvCode.Gap);
+
+                            System.Console.Write("Directory to store results.\n>");
+                            dest = System.Console.ReadLine();
+                            checker = CombineCSVFiles(csvPaths, dest);
+
+                            passFlag = true;
                             break;
 
-                        case setType.Glass:
-                            System.Console.WriteLine("Combining 2.Glass setType CSV files...");
-                            csvPaths = SearchCSV(logPath, date1, date2, carCode.CN7, setType.Gap);
-//                          CombineCSVFiles(csvPaths);
-                            flag = 1;
+                        case CsvCode.Glass:
+                            System.Console.WriteLine("Combining 2.Glass CSV files...");
+                            System.Console.WriteLine("1. CN7");
+                            csvPaths = SearchCSV(logPath, startDT, endDT, CarCode.CN7, CsvCode.Glass);
+
+                            System.Console.Write("Directory to store results.\n>");
+                            dest = System.Console.ReadLine();
+                            checker = CombineCSVFiles(csvPaths, dest);
+                            passFlag = true;
                             break;
 
-                        case setType.Vehicle:
-                            System.Console.WriteLine("Combining 3.Vehicle setType CSV files...");
-                            csvPaths = SearchCSV(logPath, date1, date2, carCode.CN7, setType.Gap);
-//                          CombineCSVFiles(csvPaths);
-                            flag = 1;
+                        case CsvCode.Vehicle:
+                            System.Console.WriteLine("Combining 3.Vehicle CSV files...");
+                            System.Console.WriteLine("1. CN7");
+                            csvPaths = SearchCSV(logPath, startDT, endDT, CarCode.CN7, CsvCode.Vehicle);
+
+                            System.Console.Write("Directory to store results.\n>");
+                            dest = System.Console.ReadLine();
+                            checker = CombineCSVFiles(csvPaths, dest);
+                            passFlag = true;
                             break;
                     }
-                } while (flag == 0);
+                } while (passFlag == false);
             }
+
             System.Console.WriteLine("Press any key to EXIT.");
             System.Console.ReadKey();
 		}
 
 
-        static System.Collections.Generic.List<string> SearchCSV(string logPath, string startDate, string endDate, carCode code, setType type)
+        static System.Collections.Generic.List<string> SearchCSV(string logPath, System.DateTime startDT, System.DateTime endDT, CarCode car, CsvCode csv) // input datetime
         {
             System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(logPath);
-            System.Collections.Generic.List<string> csvPaths = new System.Collections.Generic.List<string>();
-            bool flag = false;
+            System.Collections.Generic.List<string> datePaths = new System.Collections.Generic.List<string>();
+            bool beginFlag = false;
+            bool endFlag = false;
 
-            // 모든 DIR을 이름 순서(= 날짜 순)로 정렬하여 list에
             var directoryNames = di.EnumerateDirectories()
                     .OrderBy(d => d.Name)
                     .Select(d => d.Name)
                     .ToList();
 
-            // startDate와 비교하여 리스트에
+            int refNum = directoryNames.Count - 1;
+            int cenNum = refNum / 2;
+            double x = 0;
 
-            System.DateTime startDT;
-            System.DateTime endDT;
-            System.DateTime compareDT;
+            for (;beginFlag == false;)
+            {
+                System.DateTime.TryParseExact(directoryNames[cenNum], "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out System.DateTime centerDT);
+                if (System.DateTime.Compare(startDT, centerDT) == 0 || cenNum <= 0 || cenNum >= directoryNames.Count - 1)
+                {
+                    datePaths.Add(directoryNames[cenNum]);
+                    beginFlag = true;
+                    break;
+                }
+                else if (System.DateTime.Compare(startDT, centerDT) < 0) // startDT is later!
+                {
+                    refNum = cenNum;
+                    cenNum = cenNum / 2;
+                    System.Console.WriteLine("yyyyyyy :" + cenNum);
+                }
+                else if (System.DateTime.Compare(startDT, centerDT) > 0) // startDT is earlier.
+                {
+                    x = ((double)refNum - (double)cenNum) / 2 + 0.5;
+                    cenNum += (int)x;
+                    System.Console.WriteLine("xxxxxxxxx :"+ cenNum);
+                }
+            }
 
-            System.DateTime.TryParseExact(startDate, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out startDT);
-            System.DateTime.TryParseExact(endDate, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out endDT);
+
+            for (;endFlag == false;)
+            {
+                cenNum++;
+
+                if (cenNum >= directoryNames.Count)
+                {
+                    endFlag = true;
+                    break;
+                }
+
+                System.DateTime.TryParseExact(directoryNames[cenNum], "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out System.DateTime compareDT);
+                if (System.DateTime.Compare(endDT, compareDT) >= 0)
+                {
+                    datePaths.Add(directoryNames[cenNum]);
+                }
+                else
+                {
+                    endFlag = true;
+                    break;
+                }
+            }
 
 
+            // Print the list.
+            foreach (var datePath in datePaths)
+            {
+                System.Console.WriteLine(datePath);
+            }
+
+            System.Console.WriteLine("Press Ctrl+C key to EXIT.");
+            System.Console.ReadKey();
+
+
+
+
+
+            /*
             for (int i = 0; i < directoryNames.Count; i++)
             {
-                System.DateTime.TryParseExact(directoryNames[i], "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out compareDT);
+                System.DateTime.TryParseExact(directoryNames[i], "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out System.DateTime compareDT);
 
-                if (System.DateTime.Compare(startDT, compareDT) <= 0 && flag == false)
+                if (System.DateTime.Compare(startDT, compareDT) <= 0 && beginFlag == false)
                 {
-                    System.Console.WriteLine("Start! : " + directoryNames[i]);
-					csvPaths.Add(directoryNames[i]);
-                    flag = true;
-               	}
-                else if (System.DateTime.Compare(compareDT, endDT) <= 0 && flag == true)
+                    datePaths.Add(directoryNames[i]);
+                    beginFlag = true;
+                }
+                else if (System.DateTime.Compare(compareDT, endDT) <= 0 && beginFlag == true)
                 {
-                    System.Console.WriteLine("… : " + directoryNames[i]);
-                    csvPaths.Add(directoryNames[i]);
+                    datePaths.Add(directoryNames[i]);
                 }
                 else if (System.DateTime.Compare(compareDT, endDT) > 0)
                 {
                     System.Console.WriteLine("Done.");
                     break;
-				}
+		       }
             }
+            */
 
-            string searchType = string.Empty;
-            string searchCode = string.Empty;
-            switch (code)
+            string csvPath = string.Empty;
+            string carPath = string.Empty;
+            switch (car)
             {
-                case carCode.CN7:
-                    searchCode = @"\\CN7";
+                case CarCode.CN7:
+                    carPath = "/CN7";
                     break;
-                case carCode.DH:
-                    searchCode = @"\\DH";
+                case CarCode.DH:
+                    carPath = "/DH";
                     break;
-                case carCode.DN8:
-                    searchCode = @"\\DN8";
+                case CarCode.DN8:
+                    carPath = "/DN8";
                     break;
-                case carCode.TL:
-                    searchCode = @"\\TL";
+                case CarCode.TL:
+                    carPath = "/TL";
                     break;
             }
 
-            switch (type)
+            switch (csv)
             {
-                case setType.Gap:
-                    searchType = @"\\gap_values.csv";
+                case CsvCode.Gap:
+                    csvPath = "/gap_values.csv";
                     break;
-                case setType.Glass:
-                    searchType = @"\\glass_shift_values.csv";
+                case CsvCode.Glass:
+                    csvPath = "/glass_shift_values.csv";
                     break;
-                case setType.Vehicle:
-                    searchType = @"\\vehicle_shift_values.csv";
+                case CsvCode.Vehicle:
+                    csvPath = "/vehicle_shift_values.csv";
                     break;
             }
 
-            for (int j = 0; j < csvPaths.Count; j++)
+            System.Collections.Generic.List<string> resultPaths = new System.Collections.Generic.List<string>();
+            for (int j = 0; j < datePaths.Count; j++)
             {
-                csvPaths[j] = logPath + csvPaths[j] + searchCode + searchType;
-                System.Console.WriteLine("checking! : " + csvPaths[j]);
+                resultPaths.Add(logPath + datePaths[j] + carPath + csvPath);
+                System.Console.WriteLine("{0}.Path : {1} ", j+1, datePaths[j]);
             }
 
-            System.Console.ReadKey();
-            return csvPaths;
+            return resultPaths;
         }
 
 
-
-
-
-
-
-        static void CombineCSVFiles(System.Collections.Generic.List<string> csvPaths, string resultPath) // output 경로 입력받기
+        static bool CombineCSVFiles(System.Collections.Generic.List<string> csvPaths, string resultPath)
         {
-            System.IO.StreamWriter fileDest = new System.IO.StreamWriter(@"D:\CombineCSV\test.csv", true); // 경로 입력받기
-            int i = 0;
+            System.IO.StreamWriter fileDest = new System.IO.StreamWriter(resultPath, true);
 
-            foreach (string count in csvPaths)
+            for(int i = 0; i < csvPaths.Count; i++)
             {
                 string file = csvPaths[i];
                 if (file == null)
                 {
-                    System.Console.WriteLine("Error, CSV file is missing.");   // 폴더 내 Combine 할 csv 파일이 없음 → 종료
-                    System.Environment.Exit(0);
+                    System.Console.WriteLine("Error, CSV file is missing.");
+                    return false;
                 }
 
                 string[] lines = System.IO.File.ReadAllLines(file);
-
                 if (i > 0)
                 {
-                    lines = lines.Skip(1).ToArray();    // for문 쓰기
+                    lines = lines.Skip(1).ToArray();
                 }
 
                 foreach (string line in lines)
                 {
                     fileDest.WriteLine(line);
                 }
-                i++;
 			}
             fileDest.Close();
+            return true;
 		}
     }
 }
@@ -219,31 +273,14 @@ namespace CombineCSV        // 재사용성을 고려할 것
 
 
 
-
-
-
-
-
-
-
-
-
-
-//               System.Console.WriteLine("ROOT PATH(logPath) : " + di.Name);
-//               System.Console.WriteLine("LOGS COUNT : " + di.GetDirectories().Length);
-
-//for (int a = 0; a < directories.Count; a++)
-//{    System.Console.WriteLine(directories[a]);}
-
-
 /*
             int j = di.GetDirectories().Length; // 총 날짜 폴더 개수
-            string[] csvPaths = new string[i]; // 함수 안에서 만들기-
+            string[] csvPaths = new string[i];
 
             int x = 0;
             // foreach 구문을 이용하여 폴더 내부에 있는 폴더정보(GetDirectories()를 이용해)를 가져옵니다.
             System.Console.WriteLine("-------------------------------------");
-            System.Console.WriteLine("CSV NAME : " + searchType);
+            System.Console.WriteLine("CSV NAME : " + csvPath);
 
             foreach (var item in di.GetDirectories())
             {
@@ -252,10 +289,10 @@ namespace CombineCSV        // 재사용성을 고려할 것
                 foreach (var fi in item.GetFiles())
                 {
                     string fileName = System.IO.Path.GetFileName(fi.Name);
-                    if (fileName == searchType)
+                    if (fileName == csvPath)
                     {
                         csvPaths[x] = fi.FullName; // 차종이 2개면..
-                        x++; // 리스트로
+                        x++;
                     }
                 }
             }
